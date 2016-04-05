c415af740c2e8077d0bb74bac255a608867028db
Maxime Bury <maxime.bury@gmail.com>
2016-04-05 13:48:29 -0400

    Improve CLI interface, README

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # hipchat-dump
-[![](https://circleci.com/gh/Bowbaq/hipchat-dump/tree/master.svg?&style=shield&circle-token=f1e69183a5aababcc75d5313890189ce6e5a1e2b)](https://circleci.com/gh/Bowbaq/hipchat-dump/tree/master)
+[![](https://circleci.com/gh/Bowbaq/hipchat/tree/master.svg?&style=shield&circle-token=f1e69183a5aababcc75d5313890189ce6e5a1e2b)](https://circleci.com/gh/Bowbaq/hipchat/tree/master)
 
 Quick tool to get a dump of private (1-1) hipchat conversations
 
-Usage: `hipchat-dump -token <your api token>`
+## Installation
 
-You can get an API token [here](https://www.hipchat.com/account/api). It must have at least the `view_group` and `view_messages` groups.
+If you have `go` installed, a simple `go get github.com/Bowbaq/hipchat` should do the trick.
+
+If not, head to the [release section](https://github.com/Bowbaq/hipchat/releases) and download the binary
+for your machine. Some common platforms:
+
+| Platform    |                 Binary name |
+| ----------- | --------------------------: |
+| **OSX**     |      `hipchat_darwin_amd64` |
+| **Linux**   |       `hipchat_linux_amd64` |
+| **Windows** | `hipchat_windows_amd64.exe` |
+
+Usage: `hipchat dump -t <your api token>`
+
+You can get an API token [here](https://www.hipchat.com/account/api). It must have at least the `view_group`
+and `view_messages` groups.
---
+++ hipchat.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/codegangsta/cli"
+	"github.com/tbruyelle/hipchat-go/hipchat"
+)
+
+var (
+	Version     string
+	apiPageSize = 1000
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "hipchat"
+	app.Usage = "Archive your HipChat private messages and search them"
+	app.HideVersion = true
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "dump",
+			Aliases: []string{"d"},
+			Usage:   "Archive your HipChat private messages",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "token, t",
+					Usage: "(required) HipChat auth token with view_group, view_messages scope.\n\tSee https://www.hipchat.com/account/api",
+				},
+				cli.StringFlag{
+					Name:  "filename, f",
+					Value: "dump.json",
+					Usage: "Path of the file where the archive will be written",
+				},
+			},
+			Action: func(c *cli.Context) {
+				if !c.IsSet("token") {
+					cli.ShowSubcommandHelp(c)
+					return
+				}
+				check(dumpMessages(c.String("token"), c.String("filename")))
+				fmt.Println("Archive was written at", c.String("filename"))
+			},
+		},
+		{
+			Name:    "search",
+			Aliases: []string{"s"},
+			Usage:   "Search your archive of HipChat private messages. You must use `hipchat dump` first",
+			Action: func(c *cli.Context) {
+				fmt.Println("Not implemented yet")
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
+
+func dumpMessages(token, filename string) error {
+	h := hipchat.NewClient(token)
+
+	users, err := getUsers(h)
+	if err != nil {
+		return err
+	}
+
+	conversations := make(map[string][]*hipchat.Message)
+	for _, user := range users {
+		conversations[strconv.Itoa(user.ID)] = getMessages(h, user.ID)
+	}
+
+	encoded, err := json.MarshalIndent(map[string]interface{}{
+		"users":         users,
+		"conversations": conversations,
+	}, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filename, encoded, 0655)
+}
+
+func getUsers(h *hipchat.Client) ([]hipchat.User, error) {
+	fmt.Print("Getting users")
+	opt := &hipchat.UserListOptions{
+		ListOptions: hipchat.ListOptions{
+			MaxResults: apiPageSize,
+		},
+	}
+	users, res, err := h.User.List(opt)
+	for res.StatusCode == 429 { // Retry while rate-limited
+		fmt.Printf(" - rate-limited, sleeping for 15s\nGetting users")
+		time.Sleep(15 * time.Second)
+		users, res, err = h.User.List(opt)
+	}
+	fmt.Printf(" - Done [%d]\n", len(users))
+
+	return users, err
+}
+
+func getMessages(h *hipchat.Client, userID int) []*hipchat.Message {
+	fmt.Printf("Getting messages for %d", userID)
+
+	uniqueMessages := getMessagesPage(h, userID, "recent", 0)
+	if len(uniqueMessages) == 0 {
+		fmt.Println(" - Done [0]")
+		return []*hipchat.Message{}
+	}
+
+	now := time.Now().Add(-1 * time.Minute).UTC().Format(time.RFC3339)
+	start := 0
+	for {
+		page := getMessagesPage(h, userID, now, start)
+		for _, msg := range page {
+			uniqueMessages[msg.ID] = msg
+		}
+
+		if len(page) < apiPageSize {
+			break
+		}
+
+		start += len(page) - 1
+	}
+
+	var messages []*hipchat.Message
+	for _, msg := range uniqueMessages {
+		messages = append(messages, msg)
+	}
+	sort.Sort(byMostRecent(messages))
+	fmt.Printf(" - Done [%d]\n", len(messages))
+
+	return messages
+}
+
+func getMessagesPage(h *hipchat.Client, userID int, date string, startIndex int) map[string]*hipchat.Message {
+	u := fmt.Sprintf("user/%d/history", userID)
+	opt := &hipchat.HistoryOptions{
+		ListOptions: hipchat.ListOptions{
+			MaxResults: apiPageSize,
+			StartIndex: startIndex,
+		},
+		Date:    date,
+		Reverse: false,
+	}
+
+	req, err := h.NewRequest("GET", u, opt, nil)
+	if err != nil {
+		log.Println(req.URL.String(), err)
+		return nil
+	}
+
+	var result hipchat.History
+	res, err := h.Do(req, &result)
+	for res.StatusCode == 429 { // Retry while rate-limited
+		fmt.Printf(" - rate-limited, sleeping for 15s\nGetting messages for %d", userID)
+		time.Sleep(15 * time.Second)
+		res, err = h.Do(req, &result)
+	}
+	if err != nil {
+		log.Println(req.URL.String(), err)
+		return nil
+	}
+
+	messages := make(map[string]*hipchat.Message)
+	for i, msg := range result.Items {
+		messages[msg.ID] = &result.Items[i]
+	}
+
+	return messages
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+type byMostRecent []*hipchat.Message
+
+func (msgs byMostRecent) Len() int           { return len(msgs) }
+func (msgs byMostRecent) Less(i, j int) bool { return msgs[i].Date > msgs[j].Date }
+func (msgs byMostRecent) Swap(i, j int)      { msgs[i], msgs[j] = msgs[j], msgs[i] }
